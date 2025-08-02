c66758d331c177b0ecf45590421f622dfdfb164e
Austin Sofaer <164747892+V4N1LLA-1CE@users.noreply.github.com>
2025-08-02 13:14:28 +1000

    init proj

---
+++ colours/xcodedark.lua
@@ -0,0 +1,1 @@
+require("colours.xcodedark").load()
---
+++ lua/xcodedark/init.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.load()
+	-- clear existing highlights
+	vim.cmd("highlight clear")
+	if vim.fn.exists("syntax_on") then
+		vim.cmd("syntax reset")
+	end
+
+	vim.g.colours_name = "xcodedark"
+
+	local colours = require("colours.xcodedark")
+
+	require("xcodedark.groups.editor").setup(colours)
+	require("xcodedark.groups.syntax").setup(colours)
+	require("xcodedark.groups.integrations.treesitter").setup(colours)
+	require("xcodedark.groups.integrations.lsp").setup(colours)
+end
+
+function M.setup(opts)
+	opts = opts or {}
+	M.load()
+end
+
+return M
